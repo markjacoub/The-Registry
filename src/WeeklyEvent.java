f4f3036a88fbfc8d88f1751ef22a3fbb24c443ef
Mark <markfikry@aucegypt.edu>
2019-10-25 18:43:22 +0200

    finished weekly

--- src/WeeklyEvent.java
+++ src/WeeklyEvent.java
@@ -1,34 +1,69 @@
-import java.sql.Time;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Locale;
-/*public class WeeklyEvent extends AbstractEvent{
 
-    private Date end,start,limit;
-    private String description;
-    private Date currentDate = new  Date();
+public class WeeklyEvent extends AbstractEvent{
 
-    public void WeeklyEvent(String description, Date start, Date end, Date limit) {
+    private Date limit;
+    private int calls; //Number of times nextOccurence method hav been called
+
+    //Class constuctor
+    WeeklyEvent(String description, Date start, Date end,Date limit) {
         super(description, start, end);
         this.limit = limit;
+        this.calls = 0;
     }
 
+    //TODO:Testing
     @Override
     public Boolean hasMoreOccurences() {
+        Calendar cal =Calendar.getInstance();
+        cal.setTime(getStart());
+        cal.add(Calendar.DATE,calls*7);
 
+        if (limit.compareTo(cal.getTime()) > 0 )
+            return true;
+        else
+            return false ;
     }
 
     @Override
     public Date nextOccurence() {
-        return null;
+        /*THe approach of using 'Date' & 'Calendar' instead of 'LocalDate' library is outdated,
+        Since JV8: https://stackoverflow.com/questions/23307324/how-do-i-add-2-weeks-to-a-date-in-java
+        */
+        Date start = getStart();//Get the start date
+        Calendar cal =Calendar.getInstance();//Calendar instance
+        cal.setTime(start); //set cal = start
+
+        if(hasMoreOccurences()) {
+           if(calls == 0){ //first call return start
+               calls++; //increment for the next call
+               return start;
+           }
+            cal.add(Calendar.DATE,calls*7);//increment cal by weeks
+            calls++;//increment the number calls
+            return cal.getTime();
+        } else
+            return null;
     }
+
+    //Reset the call counter
+    @Override
+    public void init() { this.calls = 0;}
+
+    //Setters and getters
+    public Date getLimit() { return limit; }
+
+    public void setLimit(Date limit) { this.limit =limit; }
+
+    public int getCalls() { return calls; }
+
+    public void setCalls(int calls) { this.calls = calls; }
 
     @Override
-    public void init() {
-
+    public String toString() {
+        return ("Description=" + getDescription() + ", startTime=" + getStart().toString() + ", endTime=" + getEnd().toString() +
+                ", limitTime= " + getLimit().toString());
     }
 }
-*/
 
